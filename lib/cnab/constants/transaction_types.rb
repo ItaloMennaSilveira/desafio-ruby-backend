3146e8e2794b9c3e9b10a6f7e508e32840ea3a29
Italo Silveira <italo.menna.silveira@gmail.com>
2020-11-10 20:42:08 -0300

    feat(lib/cnab): Create modulo as constant to get transaction type from file

---
+++ lib/cnab/constants/transaction_types.rb
@@ -0,0 +1,15 @@
+module CNAB
+  module Constants
+    class TransactionTypes
+      DEBITO = '1'
+      BOLETO = '2'
+      FINANCIAMENTO = '3'
+      CREDITO = '4'
+      RECEBIMENTO_EMPRESTIMO = '5'
+      VENDAS = '6'
+      RECEBIMENTO_TED = '7'
+      RECEBIMENTO_DOC = '8'
+      ALUGUEL = '9'
+    end
+  end
+end
