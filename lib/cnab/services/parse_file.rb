baf7b20f3c22b5d2a55e1929e950c29c3f23fcf0
Italo Silveira <italo.menna.silveira@gmail.com>
2020-11-10 21:30:49 -0300

    feat(lib/cnab): Save info when parse file is called

--- lib/cnab/services/parse_file.rb
+++ lib/cnab/services/parse_file.rb
@@ -11,7 +11,26 @@ module CNAB
         file_content.each_with_index do |line, _index|
           parse = @parse_line.call(line)
 
+          shop = Shop.where(name: parse.data[:shop_name])
+                     .first_or_create!(
+                       name: parse.data[:shop_name]
+                     )
 
+          Beneficiary.where(cpf: parse.data[:cpf])
+                     .first_or_create!(
+                       name: parse.data[:shop_owner],
+                       cpf: parse.data[:cpf],
+                       shop_id: shop.id
+                     )
+
+          Transaction.create!(
+            transaction_type: parse.data[:type],
+            occurrence_date: parse.data[:occurrence_date],
+            value: parse.data[:value],
+            credit_card: parse.data[:card],
+            hour: parse.data[:hour],
+            shop_id: shop.id
+          )
         end
       end
     end
