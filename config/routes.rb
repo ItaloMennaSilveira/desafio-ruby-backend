babd7dc0b4db00a2c956d639ae091b8875397a1a
Italo Silveira <italo.menna.silveira@gmail.com>
2020-11-10 21:25:44 -0300

    feat(routes): Add routes to shops controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "shops#index"
+  get "index" to: "shops#index"
+  post "upload-file", to: "shops#upload_file"
 end
