f0d2b78b582a8bb6711f05c6751ae729e4720659
Italo Silveira <italo.menna.silveira@gmail.com>
2020-11-10 21:18:29 -0300

    feat(models/shop): Add balance to shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -3,4 +3,11 @@ class Shop < ApplicationRecord
   has_one :beneficiary
 
   validates :name, uniqueness: true
+
+  def balance
+    positive = self.transactions.positive_transactions.sum("value")
+    negative = self.transactions.negative_transactions.sum("value")
+
+    positive - negative
+  end
 end
